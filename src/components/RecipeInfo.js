6bd53d70830dc5dd7effa7b81ed46234429330af
Sam Barrick <you@example.com>
2020-05-06 14:35:45 -0500

    textbox changes made to RecipeInfo.js

--- src/components/RecipeInfo.js
+++ src/components/RecipeInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Container } from "@material-ui/core";
-import EditIcon from '@material-ui/icons/Edit';
+import { Container, TextField, Tooltip } from "@material-ui/core";
+import SaveIcon from '@material-ui/icons/Save';
+
 
 const RecipeInfo = (props) => {
   // const name = props.match.params.name
@@ -12,8 +13,33 @@ const RecipeInfo = (props) => {
  <h2> <span className="recipe-description-span">Recipe Name: </span>{/* recipe.recipe_name */}</h2>
  <h4> <span className="recipe-description-span">Cuisine Type: </span>{/* recipe.cuisine_type */}</h4>
  <h4> <span className="recipe-description-span">Cook Time: </span>{/* recipe.total_cook_time */}</h4>
- <h4> <span className="recipe-description-span">Ingredients: </span>{/* recipe.ingredients */}<EditIcon className="edit-recipe-icon"/></h4>
- <h4> <span className="recipe-description-span">Directions: </span>{/* recipe.directions */}<EditIcon className="edit-recipe-icon"/></h4>
+ <h4> <span className="recipe-description-span">Ingredients: </span>
+
+ <TextField
+ id="outlined-textarea"
+ label="Edit ingredients info"
+ placeholder="Edit ingredients info"
+ multiline
+ variant="outlined"
+/>
+<Tooltip title="Save">
+<SaveIcon className="edit-recipe-icon"/>
+</Tooltip>
+</h4>
+
+ <h4> <span className="recipe-description-span">Directions: </span>
+
+ <TextField
+ id="outlined-textarea"
+ label="Edit directions info"
+ placeholder="Edit directions info"
+ multiline
+ variant="outlined"
+ tooltip="Save"
+/>
+<Tooltip title="Save">
+<SaveIcon className="edit-recipe-icon"/>
+</Tooltip></h4>
 </Container>
   );
 };
